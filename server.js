89608cb6b4acf187cfaf2ae63b1c9df748c59c0b
Nikunj Dhingra <35530096+Nikunj2102@users.noreply.github.com>
2018-03-05 21:40:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,10 +80,11 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-// articleName == article-one
+
+app.get('/:articleName',function(req ,res){
+    // articleName == article-one
 // articles[articleName] == {} object for article one
 var articleName= req.params.articleName;
-app.get('/:articleName',function(req ,res){
   res.send(createTemplate(articles[articleName]));
 });
 
