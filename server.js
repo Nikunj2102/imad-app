595dce820262806de9892b3b244465490cd81a9f
Nikunj Dhingra <35530096+Nikunj2102@users.noreply.github.com>
2018-03-11 03:13:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require("pg").Pool;
+
+var config = {
+  user:"nikunjdhingra21",
+  database:"nikunjdhingra21",
+  host:"db.imad.hasura-app.io",
+  port:"5432",
+  password: process.env.DB_PASSWORD
+      
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -85,6 +95,25 @@ app.get('/counter', function(req,res){
    counter=counter+1;
    res.send(counter.toString());
     
+});
+
+var Pool=new Pool(config);
+app.get("/test-db" ,function(req,res)
+{
+   //make a select request
+   //return a response with a result
+   Pool.query("SELECT * FROM test" , function(err,result)
+   {
+      if(err)
+      {
+          res.status(500).send(err.toString());
+      }
+      else
+      {
+          res.send(JSON.stringfy(result));
+      }
+   });
+   
 });
 var names=[];
 
