af7ac02cc29c80aa2f96dbc6a32221e264797d6d
Nikunj Dhingra <35530096+Nikunj2102@users.noreply.github.com>
2018-03-14 04:22:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -36,7 +36,7 @@ submit.onclick = function(){
       console.log(password);
       request.open('POST', "http://nikunjdhingra21.imad.hasura-app.io/login ", true);
       request.setRequestHeader("Content-Type" , "application/json");
-      request.send(JSON.stringfy({username: username , password: password}));
+      request.send(JSON.stringify({username: username , password: password}));
      
  
 };
