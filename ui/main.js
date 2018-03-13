2c68e36ded991a7572d89e0503c509fc93c094a1
Nikunj Dhingra <35530096+Nikunj2102@users.noreply.github.com>
2018-03-14 04:17:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,40 +1,4 @@
-// give a counter to the button
-var button =document.getElementById("counter");
-button.onclick=function(){
-    
-    //create a request to the counter end point
-    
-    var request=new XMLHttpRequest();
-    
-    //capture the response ans store it in a variable
-    request.onreadystatechange = function(){
-      if(request.readyState === XMLHttpRequest.DONE){
-          
-          // we take some action
-          if(request.status===200)
-          {     
-                //capture a list of names and renderthem as a name
-               var names=['name1','name2','name3'];
-    var list="";
-    for(var i=0;i<names.length;i++)
-    {
-        list += "<li>" + names[i] + "</li>";
-    }
-    
-    var ul=document.getElementById("namelist");
-    ul.innerHTML=list;
-          }
-        }
-          
-      };
-      
-      //make a request
-      request.open('GET', "http://nikunjdhingra21.imad.hasura-app.io/counter", true);
-      request.send(null);
-     
-};
-
-//submit name
+//submit username/password to login
 
 var submit=document.getElementById("submit_btn");
 submit.onclick = function(){
@@ -51,26 +15,28 @@ submit.onclick = function(){
           // we take some action
           if(request.status===200)
           {     
-                //capture a list of names and renderthem as a name
-               var names=request.responseText;
-               names=JSON.parse(names);
-    var list="";
-    for(var i=0;i<names.length;i++)
-    {
-        list += "<li>" + names[i] + "</li>";
-    }
-    
-    var ul=document.getElementById("namelist");
-    ul.innerHTML=list;
+              console.log("user logged in");
+              alert("logged in successffullly");
+          }
+          else if(request.status=== 403)
+          {
+              alert("invalid username/password");
+          }
+          else if(request.status===500)
+          {
+              alert("something went wrong on the server");
           }
         }
           
       };
-      var nameInput=document.getElementById("name");
-      var name = nameInput.value;
+      var username=document.getElementById("username").value;
+      var password=document.getElementById("password").value;
       //make a request
-      request.open('GET', "http://nikunjdhingra21.imad.hasura-app.io/submit-name?name=" +name , true);
-      request.send(null);
+      console.log(username);
+      console.log(password);
+      request.open('POST', "http://nikunjdhingra21.imad.hasura-app.io/login ", true);
+      request.setRequestHeader("Content-Type" , "application/json");
+      request.send(JSON.stringfy({username: username , password: password}));
      
  
 };
