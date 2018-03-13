7079f1c20d3aff725865165807434f0d988ca6f7
Nikunj Dhingra <35530096+Nikunj2102@users.noreply.github.com>
2018-03-14 03:35:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require("pg").Pool;
+var bodyParser=require("body-parser");
 var crypto = require("crypto");
 
 var config = {
@@ -15,6 +16,8 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
+
 
 function createTemplate(data)
 {
@@ -63,7 +66,7 @@ function hash(input,salt)
     //how do we create a hash?
 //    we use node crypto
 var hashed = crypto.pbkdf2Sync(input , salt, 10000 ,512,"sha512");
-return hashed.toString("hex");    
+return ["pbkdf2",10000,salt, hashed.toString("hex")].join('$');    
     
 }
 
@@ -72,6 +75,27 @@ app.get("/hash/:input", function(req,res)
  
  var hashedString = hash(req.params.input,'this-is-some-random-string'); 
  res.send(hashedString);
+});
+
+app.post("/create-user" , function(req,res)
+{   
+    var username = req.body.username;
+    var password=req.body.password;
+    var salt= crypto.randomBytes(128).toString("hex");
+    var dbString = hash(password,salt);
+    pool.query('INSERT INTO "user" (username.password) VALUES ($1,$2)',[username,dbString],function(err,result)
+    {
+         if(err)
+      {
+          res.status(500).send(err.toString());
+      }
+      else
+      {
+          res.send("user successfully created:" +username);
+      }
+    });
+    
+    
 });
 
  var counter=0;
